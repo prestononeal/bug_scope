152aecabe12ebff36c273b73f627f5cdc5e664dd
Preston O'Neal <prestononeal@gmail.com>
2016-12-13 23:01:14 -0800

    Improve db seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,70 +6,87 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Weighted list of issue templates
+ISSUE_TEMPLATES = [
+  [:fatalassert, 'thiscrashed.cpp:80'], [:fatalassert, 'thisalsocrashed.c:76'], 
+  [:fatalassert, 'testapp.c:10'], [:fatalassert, 'helloworld.cpp:1000'],
+  [:fatalassert, 'crashycrash.c:108'], [:fatalassert, 'weather.c:587'], 
+  [:fatalassert, 'spacebook.cpp:809'], [:fatalassert, 'nevercrashes.c:39'],
+  [:fatalassert, 'scopecore.c:512'], [:fatalassert, 'phonedriver.c:20'],
+  [:hardfault, 'PC0XDEADBEEF'], [:hardfault, 'PC0XDEAABEEF'], 
+  [:hardfault, 'PC0XDEADEEEF'], [:hardfault, 'PC0XDEADDEAD'], 
+  [:hardfault, 'PC0XDEADBEE2'], [:hardfault, 'PC0XEA4U11FF'], 
+  [:watchdog, 'XYZ_WATCHDOG'], [:watchdog, 'DSP_WATCHDOG']
+]
+
 # Create some initial builds
-Build.create! [
-  {
-    name: "1",
+1.upto 30 do |i|
+  Build.create({
+    name: i.to_s,
     branch: "dev",
     product: "coolproduct"
-  },
-  {
-    name: "2",
-    branch: "dev",
-    product: "coolproduct"
-  },
-  {
-    name: "3",
-    branch: "dev",
-    product: "coolproduct"
-  },
-  {
-    name: "1",
+  })
+end
+1.upto 20 do |i|
+  Build.create({
+    name: i.to_s,
     branch: "tst",
     product: "coolproduct"
-  },
-  {
-    name: "2",
-    branch: "tst",
-    product: "coolproduct"
-  },
-  {
-    name: "1",
+  })
+end
+1.upto 10 do |i|
+  Build.create({
+    name: i.to_s,
     branch: "prod",
     product: "coolproduct"
-  }
-]
+  })
+end
+1.upto 15 do |i|
+  Build.create({
+    name: i.to_s,
+    branch: "dev",
+    product: "nextcoolproduct"
+  })
+end
+1.upto 5 do |i|
+  Build.create({
+    name: i.to_s,
+    branch: "tst",
+    product: "nextcoolproduct"
+  })
+end
+1.upto 2 do |i|
+  Build.create({
+    name: i.to_s,
+    branch: "prod",
+    product: "nextcoolproduct"
+  })
+end
 
-# Create an issue
-Issue.create! [
-  {
-    issue_type: "fatalassert",
-    signature: "thiscrashed.c:108",
-    ticket: "jira-1"
-  },
-  {
-    issue_type: "hardfault",
-    signature: "PC0XDEADBEEF",
-    ticket: "jira-2"
-  }
-]
+# Create a bunch of random issues and instances
+# from a given build
+builds = Build.all
+20000.times {
+  it = ISSUE_TEMPLATES.sample
+  # Use first_or_create instead of create so we don't
+  # create duplicate issues for a build. Their issuetypes and signature
+  # combos should be unique.
+  # This is similar to the "report" method in the issues controller.
+  created = false
+  build = builds.sample(1)[0]
+  issue = build.issues.where(:issue_type => it[0], :signature => it[1]).first_or_create do |obj|
+    # If the issue gets created, the instance linking the issue and build will also get created
+    created = true
+  end
 
-# Create instances of these issues from the first build
-Issue.first.instances.create! [
-  {
-    build: Build.first
-  },
-  {
-    build: Build.first
-  },
-  {
-    build: Build.first
-  }
-]
+  # Create an instance that points to this build and issue if it wasn't already created
+  issue.instances.create(:build => build) if !created
+}
 
-Issue.last.instances.create! [
-  {
-    build: Build.first
-  }
-]
-
+# Create some tickets we can assign to some of the issues
+issues = Issue.all
+1.upto 500 do |i|
+  iss = issues.sample(1)[0]
+  iss.ticket = 'jira-' + i.to_s
+  iss.save
+end
