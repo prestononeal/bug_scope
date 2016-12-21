e2426a5f1185d4205cae4609e436c33d8d30f58f
Preston O'Neal <prestononeal@gmail.com>
2016-12-20 16:04:09 -0800

    Add build report graph clicking hook

--- app/assets/javascripts/build-report/build-report.controller.js
+++ app/assets/javascripts/build-report/build-report.controller.js
@@ -11,13 +11,16 @@ function BuildReportController(issues) {
 
   // Populate the chart data
   buildRepCtrl.chart = {};
-  buildRepCtrl.chart.options = {legend: {display: true}};
   buildRepCtrl.chart.labels = [];
   buildRepCtrl.chart.data = [];
   for(var i = 0; i < buildRepCtrl.issues.length; i++) {
     buildRepCtrl.chart.labels.push(buildRepCtrl.issues[i].signature);
     buildRepCtrl.chart.data.push(Number(buildRepCtrl.issues[i].instances_count));
   }
+
+  buildRepCtrl.chart.click = function(event) {
+    
+  };
 }
 
 })();
