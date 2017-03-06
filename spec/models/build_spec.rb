40a240fb83e5062080eb7cdcd306746da0a323cd
Preston O'Neal <prestononeal@gmail.com>
2017-03-05 22:30:09 -0800

    Add basic Build rspec tests

---
+++ spec/models/build_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Build, type: :model do
+  include_context "db_cleanup", :transaction
+  before(:all) do
+    @build = FactoryGirl.create(:build)
+  end
+  let(:build) { Build.find(@build.id) }
+
+  context "created Build" do
+    it { expect(build).to be_persisted }
+    it { expect(build.name).to_not be_nil }
+    it { expect(build.branch).to_not be_nil }
+    it { expect(build.product).to_not be_nil }
+    it { expect(build.created_at).to_not be_nil }
+    it { expect(build.updated_at).to_not be_nil }
+  end
+
+end
