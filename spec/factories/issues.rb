8476fe9c83e31c92d1d556a1c89377612f1f7497
Preston O'Neal <prestononeal@gmail.com>
2017-03-07 22:02:05 -0800

    Add Issue test, simplify db seeding

---
+++ spec/factories/issues.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :issue_faker, class: 'Issue' do
+    issue_type { ['watchdog', 'fatalassert', 'hardfault'].sample }
+    signature { Faker::Internet.domain_name }
+    ticket { ['JIRA-' + Faker::Number.between(1, 5).to_s, nil, nil].sample }
+    state { ['open', 'closed', nil, nil].sample }
+    note { Faker::Hacker.say_something_smart }
+  end
+
+  factory :issue, :parent=>:issue_faker do
+  end
+end
