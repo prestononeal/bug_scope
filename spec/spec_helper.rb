44bd9a0c89db13d70e7904f5a141610a8a5e590d
Preston O'Neal <prestononeal@gmail.com>
2017-03-09 22:47:04 -0800

    Add code coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+SimpleCov.start
 
 require_relative 'support/database_cleaners.rb'
 
