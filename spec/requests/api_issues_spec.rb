07204d8aca11acd6385c85d15f8b7a8f01270a60
Preston O'Neal <prestononeal@gmail.com>
2017-03-10 20:29:22 -0800

    More issue spec test coverage

--- spec/requests/api_issues_spec.rb
+++ spec/requests/api_issues_spec.rb
@@ -4,7 +4,7 @@ describe "Issue API", :request do
   include_context "db_cleanup_each"
   let(:payload) { JSON.parse(response.body) }
 
-  def validate_issue_payload
+  def validate_instance_payload_pass
     expect(response).to have_http_status(:ok)
     expect(response.content_type).to eq("application/json")
     expect(payload).to have_key("id")
@@ -13,6 +13,22 @@ describe "Issue API", :request do
     expect(payload).to have_key("created_at")
     expect(payload).to have_key("updated_at")
     expect(payload).to have_key("found_at")
+  end
+
+  def validate_issue_payload_pass
+    expect(response).to have_http_status(:ok)
+    expect(response.content_type).to eq("application/json")
+    expect(payload).to have_key("id")
+    expect(payload).to have_key("created_at")
+    expect(payload).to have_key("updated_at")
+  end
+
+  def validate_issue_payload_fail (failure_msg)
+    expect(response).to have_http_status(:unprocessable_entity)
+    expect(response.content_type).to eq("application/json")
+    expect(payload).to have_key("errors")
+    expect(payload["errors"]).to have_key("full_messages")
+    expect(payload["errors"]["full_messages"]).to include(failure_msg)
   end
 
   context "caller requests list of Issues" do
@@ -54,24 +70,20 @@ describe "Issue API", :request do
         }
       }
       post "/issues/report", params: new_issue_params
-      validate_issue_payload
+      validate_instance_payload_pass
 
-      expect(Issue.find_by(:issue_type=>iss[:issue_type], 
-                           :signature=>iss[:signature])).to_not be_nil
+      iss = Issue.find_by(:issue_type=>iss[:issue_type], 
+                          :signature=>iss[:signature])
+      expect(iss).to_not be_nil
+      expect(iss.instances).to_not be_nil
       expect(Build.find_by(:product=>bld[:product],
                            :branch=>bld[:branch],
                            :name=>bld[:name])).to_not be_nil
     end
 
     it "a new Issue with invalid params" do
-      expect(Issue.find_by(:signature=>new_issue_signature)).to be_nil
-      new_issue_params = {
-        :issue_info => {
-          :issue_type => 'fault',
-          :signature => new_issue_signature
-        }
-      }
-      post "/issues/report", params: new_issue_params
+      iss = FactoryGirl.build(:issue)
+      post "/issues/report", params: iss.attributes
       expect(response).to have_http_status(:unprocessable_entity)
       expect(response.content_type).to eq("application/json")
       expect(payload).to have_key("errors")
@@ -81,11 +93,9 @@ describe "Issue API", :request do
     end
 
     it "a known Issue" do
-      bld = Build.create(:product=>"test_product",
-                         :branch=>"test_branch",
-                         :name=>"test_name")
-      iss = bld.issues.create(:issue_type => "test_type", 
-                              :signature => "test_sig")
+      bld = FactoryGirl.create(:build)
+      iss = bld.issues.create(:issue_type=>"test_type", 
+                              :signature=>"test_sig")
       expect(iss.instances.count).to eq(1)
       known_issue_params = {
         :issue_info => {
@@ -99,20 +109,59 @@ describe "Issue API", :request do
         }
       }
       post "/issues/report", params: known_issue_params
-      validate_issue_payload
+      validate_issue_payload_pass
       
       expect(iss.instances.count).to eq(2)
     end
   end
 
-  context "caller merges" do
-    it "the same issue"
+  context "caller merges two issues," do
+    let!(:build) { FactoryGirl.create(:build) }
+    let!(:issue) { build.issues.create(:issue_type=>"test_type", :signature=>"test_sig")}
+    it "the same issue" do
+      put "/issues/#{issue.id}/merge_to", params: {:parent_id => issue.id}
+      validate_issue_payload_fail("Cannot merge an issue to itself")
+    end
 
-    it "different issues with conflicting tickets" 
+    it "with invalid params" do
+      put "/issues/#{issue.id}/merge_to", params: {}
+      validate_issue_payload_fail("Missing parameter: parent_id")
+    end
 
-    it "different issues, parent issue has ticket"
+    it "conflicting tickets" do
+      issue1 = build.issues.create(:issue_type=>"test_type",
+                                   :signature=>"test_sig",
+                                   :ticket=>"test_ticket1")
+      issue2 = build.issues.create(:issue_type=>"test_type",
+                                   :signature=>"test_sig",
+                                   :ticket=>"test_ticket2")
+      put "/issues/#{issue1.id}/merge_to", params: {:parent_id => issue2.id}
+      validate_issue_payload_fail("Issues have conflicting tickets")
+    end
 
-    it "different issues, child issue has ticket" 
+    it "parent issue has ticket" do
+      issue1 = build.issues.create(:issue_type=>"test_type",
+                                   :signature=>"test_sig",
+                                   :ticket=>"test_ticket1")
+      issue2 = build.issues.create(:issue_type=>"test_type",
+                                   :signature=>"test_sig")
+      put "/issues/#{issue1.id}/merge_to", params: {:parent_id => issue2.id}
+      validate_issue_payload_pass
+    end
+
+    it "child issue has ticket" do
+      issue1 = build.issues.create(:issue_type=>"test_type",
+                                   :signature=>"test_sig")
+      issue2 = build.issues.create(:issue_type=>"test_type",
+                                   :signature=>"test_sig",
+                                   :ticket=>"test_ticket2")
+      put "/issues/#{issue1.id}/merge_to", params: {:parent_id => issue2.id}
+      validate_issue_payload_pass
+    end
+
+    it "instances point to original issues in db"
+
+    it "parent issue info includes child instances"
   end
 
 end
