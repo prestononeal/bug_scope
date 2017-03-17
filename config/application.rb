173a27cd058fe15f5db44278c4e05d0658bd302b
Preston O'Neal <prestononeal@gmail.com>
2017-03-17 16:06:15 -0700

    Remove SSL forcing for production, use secret key base from Figaro

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Scope
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.secret_key_base = Figaro.env.secret_key_base
   end
 end
