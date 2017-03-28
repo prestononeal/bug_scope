1146d63fe5f53ee4cd62aeb8f0ea8f0794463f6a
Preston O'Neal <prestononeal@gmail.com>
2017-03-27 23:12:30 -0700

    Remove migration change to add a default to Instance found_at field

--- db/migrate/20170113222418_add_found_at_to_instances.rb
+++ db/migrate/20170113222418_add_found_at_to_instances.rb
@@ -1,15 +1,5 @@
 class AddFoundAtToInstances < ActiveRecord::Migration[5.0]
   def change
     add_column :instances, :found_at, :datetime
-    reversible do |direction|
-      direction.up do
-        # If the migration is being applied, set the found_at equal to created_at as a default
-        # Future writes will rely on the model to set the default value
-        Instance.find_each do |instance|
-          instance.found_at ||= instance.created_at
-          instance.save!
-        end
-      end
-    end
   end
 end
